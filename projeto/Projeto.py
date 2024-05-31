bfa5124ad56224fb3f7534967bd5a9ff8dd5b61d
Felipe Serra <86814247+Felipe-Serra@users.noreply.github.com>
2024-05-31 20:23:03 -0300

    Versao 1.3 - Gerador de LOG (teste avancado)

--- projeto/Projeto.py
+++ projeto/Projeto.py
@@ -15,7 +15,6 @@ def pegarNDoc():
     pg.hotkey("ctrl","y")
     pg.press("down", presses=(i+1)) #vai de 1 até o final da coluna1 (tamanho coluna SAP)
     pg.hotkey("ctrl", "c")
- 
 def pegarNData():
     pg.hotkey("ctrl","home")
     pg.keyDown("shift") #Vencimento liq - cabeçalho
@@ -26,13 +25,10 @@ def pegarNData():
     pg.press("down", presses=(i+1)) #vai de 1 até o final da coluna1 (tamanho coluna SAP)
     pg.hotkey("ctrl", "c")
  
- 
- 
- 
 permissao=0
-supdoc, supbloq, supdata = 0
+ 
 tabela = pd.read_excel("Projeto de automacao SAP.xlsx", sheet_name="DZ - Juros")
-tabela = tabela.drop(columns=["Nosso Número", "Data da Liquidação", "Pagador","Conta Cobrança", "Tipo Cobrança / Modalidade", "Responsável"])
+tabela = tabela.drop(columns=["Nosso Número", "Data da Liquidação", "Pagador", "Responsável"])
 print(tabela)
  
 pg.hotkey("alt", "tab")
@@ -41,6 +37,9 @@ pg.press("enter")
 time.sleep(0.5)
  
 for linha in tabela.index:
+    supbloq= 0
+    supdata= 0
+ 
     # Selecionar cliente na FBL5N
     pg.write(str(tabela.loc[linha, "Cod Sap"]))
     pg.press("f8") #Rodar
@@ -77,15 +76,16 @@ for linha in tabela.index:
         new_tabela = pd.concat([new_tabela, compData], axis=1)
         print(new_tabela)
  
-       
-   
         # Comparando Ndoc e ref
         numDoc=float(new_tabela.loc[linha,"Seu Número"])
         numRef=float(new_tabela.loc[linha,"Comp Doc"])
+ 
         if(numDoc == numRef): #Se Doc=ref
+ 
             # Comparando data de vencimento
             dataDoc=new_tabela.loc[linha,"Vencimento"]
             dataRef=new_tabela.loc[linha,"Comp Data"]
+ 
             if(dataRef==dataDoc):            
                 permissao=1 #Permissão Para rodar Script do SAP
                 fatura=new_tabela.loc[linha,"Seu Número"]
@@ -115,7 +115,6 @@ for linha in tabela.index:
             print("Doc diferentes na tabela SAP em:",(i+1))
             print(new_tabela.loc[linha])
            
-           
         i=i+1
  
     pg.press("f3")
@@ -125,20 +124,50 @@ for linha in tabela.index:
  
         print("\nRodar script em\n",fatura)
         permissao=0
+ 
+    # LOG de dados -Tabela de retorno
+    book = load_workbook("Projeto de automacao SAP.xlsx")
+    sheet = book["Retorno"]
+    book.close()
    
-    #Fatura não encontrada - tabela de retorno
-    i=0
-    index=tabela.index.any() +1
-    if (supbloq & supdata & supdoc == 0):
-        while i <= index:
-            book = load_workbook("Projeto de automacao SAP.xlsx")
-            sheet = book["Retorno"]
-            writer = pd.ExcelWriter('Projeto de automacao SAP.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')        
+    if book.close() == None:
+        # Fatura não encontrada
+        if (supbloq & supdata  == 0):
+            writer = pd.ExcelWriter('Projeto de automacao SAP.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
+ 
+            linha2=tabela.loc[[linha]]
            
-            linha2=tabela.iloc[[i]]
-            linha2.to_excel(writer, sheet_name="Retorno", startrow=1+i, header=None)
-            i+=1
+            linha2.to_excel(writer, sheet_name="Retorno", header=None, index=None, startrow=linha+2, startcol=1)
             writer.close()
+            print(book.close())
             print(linha2)
+ 
+        # Fatura com data divergencia
+        if (supbloq==1 & supdata==0):
+            writer = pd.ExcelWriter('Projeto de automacao SAP.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
+ 
+            linha2=tabela.loc[[linha]]
+           
+            linha2.to_excel(writer, sheet_name="Retorno", header=None, index=None, startrow=linha+2, startcol=11)
+            writer.close()
+            print(book.close())
+            print(linha2)
+ 
+        # Fatura baixada
+        if ((supbloq==1 | supbloq==0) & supdata==1):
+            writer = pd.ExcelWriter('Projeto de automacao SAP.xlsx', engine='openpyxl', mode='a', if_sheet_exists='overlay')
+ 
+            linha2=tabela.loc[[linha]]
+           
+            linha2.to_excel(writer, sheet_name="Retorno", header=None, index=None, startrow=linha+2, startcol=22)
+            writer.close()
+            print(book.close())
+            print(linha2)
+    else:
+        print("Excel aberto: feche Aplicativo")
+   
+ 
+ 
+    book.close()
  
 pg.hotkey("alt", "tab")
